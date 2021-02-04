0875751c108c15f31425e7148ff3a7c639540d89
taeeunn <xodms8713@gmail.com>
2021-02-05 00:55:46 +0900

    feat: cover page update

---
+++ app/src/main/java/com/knowhow/android/picturewithai/Initial.java
@@ -0,0 +1,19 @@
+package com.knowhow.android.picturewithai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Initial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial);
+
+
+
+    }
+}
