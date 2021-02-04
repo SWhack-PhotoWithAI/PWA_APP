66fac9cc552a677db5a45dfb43575f2b786adb36
taeeunn <xodms8713@gmail.com>
2021-02-05 01:39:16 +0900

    feat: 화면 전

--- app/src/main/java/com/knowhow/android/picturewithai/Initial.java
+++ app/src/main/java/com/knowhow/android/picturewithai/Initial.java
@@ -2,9 +2,12 @@ package com.knowhow.android.picturewithai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class Initial extends AppCompatActivity {
 
@@ -13,7 +16,14 @@ public class Initial extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
 
-
+        LinearLayout app_layer = (LinearLayout) findViewById (R.id.cover);
+        app_layer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
