80926036f89a7d65dd2d7c38d3fb3ef9bc64b3f9
SuyeonChoi <tndus970120@gmail.com>
2021-02-05 15:19:17 +0900

    add get method /upload

--- src/main/java/knowhow/photowithai/controller/ImageApiController.java
+++ src/main/java/knowhow/photowithai/controller/ImageApiController.java
@@ -1,8 +1,22 @@
 package knowhow.photowithai.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Controller
 public class ImageApiController {
+
+    @GetMapping("/upload")
+    String upload() {
+        return "upload";
+    }
+
+
 
 }
