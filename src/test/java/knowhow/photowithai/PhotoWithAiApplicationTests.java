ed6ca703e0134f9742d4afc8d2abd9032ca0b8d0
SuyeonChoi <tndus970120@gmail.com>
2021-02-05 14:53:49 +0900

    Initialize server

---
+++ src/test/java/knowhow/photowithai/PhotoWithAiApplicationTests.java
@@ -0,0 +1,13 @@
+package knowhow.photowithai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoWithAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
